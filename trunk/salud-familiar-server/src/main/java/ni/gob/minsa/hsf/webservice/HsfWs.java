1407daf9e85c3516e5de55e34ac759f417ec2754
wravmon@gmail.com <wravmon@gmail.com@c17d4f34-b528-7a3a-2556-87d1dd14beef>
2015-01-19 15:47:19 +0000

    Latest code

---
+++ trunk/salud-familiar-server/src/main/java/ni/gob/minsa/hsf/webservice/HsfWs.java
@@ -0,0 +1,38 @@
+package ni.gob.minsa.hsf.webservice;
+
+import javax.annotation.Resource;
+
+import ni.gob.minsa.hsf.domain.ws.PersonChs;
+import ni.gob.minsa.hsf.service.PersonChsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Controlador web de peticiones relacionadas a web services
+ * 
+ * @author William Avilés
+ */
+@Controller
+@RequestMapping("/ws/*")
+public class HsfWs {
+	private static final Logger logger = LoggerFactory.getLogger(HsfWs.class);
+	@Resource(name="personChsService")
+	private PersonChsService personChsService;
+	
+	/**
+     * Retorna una CHS en base a una persona. Acepta una solicitud GET para JSON
+     * @return Una CaractHigSanitarias JSON.
+     */
+    @RequestMapping(value = "persona", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody PersonChs fetchPersonChsJson(@RequestParam(value = "cedula", required = true) String cedula) {
+        logger.info("Obteniendo la respuesta en JSON");
+        PersonChs personChs = personChsService.getData(cedula);
+        return personChs;	
+    }
+
+}
